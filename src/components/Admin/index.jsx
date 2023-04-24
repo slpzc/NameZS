3d46045c8a99b60039c94270c84aa785714b6c55
xEBUN <leaveyt@mail.ru>
2023-04-25 00:10:12 +0500

    last release

---
+++ src/components/Admin/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Main } from './Admin.styled'
+const Admin = () => {
+    return (
+        <Main>
+            <h2>Добавление проектов</h2>
+        </Main>
+    );
+};
+
+export default Admin;
