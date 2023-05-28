6f9b5b9bebe1880aef1ebbc243ec3824644feb11
xEBUN <73482583+xEBUN@users.noreply.github.com>
2023-05-28 14:07:30 +0500

    Update Admin.jsx
    Passing admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AdminComponent from '@components/Admin/'
 const Admin = () => {
-    return <AdminComponent/>;
+    const [showComponent, setShowComponent] = useState(false);
+
+  useEffect(() => {
+
+    const email = prompt('Введите почту:');
+
+    const password = prompt('Введите пароль:');
+
+    // Проверка введенных данных
+
+    if (email === 'namezsprod@mail.ru' && password === 'EBT-vdy-v3x-RA9') {
+
+      setShowComponent(true);
+
+    } else {
+
+      setShowComponent(false);
+
+    }
+
+  }, []);
+    return (
+<>
+        { showComponent ?
+<AdminComponent/> : "Nothing here" }
+        </>
 };
 
 export default Admin;
